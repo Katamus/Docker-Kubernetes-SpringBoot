264bcfc9c913b7460b9d50a2a559b4090d752f8b
Christian <christiancatamuscay@r-fast.com>
2024-01-11 16:12:29 -0500

    16. RestController y metodos handler DELETE

--- msvc-usuarios/src/main/java/org/catamus/springcloud/msvc/usuarios/controllers/UsuarioController.java
+++ msvc-usuarios/src/main/java/org/catamus/springcloud/msvc/usuarios/controllers/UsuarioController.java
@@ -50,6 +50,16 @@ public class UsuarioController {
         return ResponseEntity.notFound().build();
     }
 
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> eliminar(@PathVariable Long id){
+        Optional<Usuario> o = service.porId(id);
+        if(o.isPresent()){
+            service.eliminar(id);
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
+
 
 
 }
