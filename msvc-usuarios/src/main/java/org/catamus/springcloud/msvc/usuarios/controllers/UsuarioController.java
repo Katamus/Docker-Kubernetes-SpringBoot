4c6bad39a03ec8c0040fc3c850ea04fed7927839
Christian <christiancatamuscay@r-fast.com>
2024-01-11 16:05:45 -0500

    15. RestController y metodos handler POST y PUT

--- msvc-usuarios/src/main/java/org/catamus/springcloud/msvc/usuarios/controllers/UsuarioController.java
+++ msvc-usuarios/src/main/java/org/catamus/springcloud/msvc/usuarios/controllers/UsuarioController.java
@@ -3,10 +3,9 @@ package org.catamus.springcloud.msvc.usuarios.controllers;
 import org.catamus.springcloud.msvc.usuarios.models.entity.Usuario;
 import org.catamus.springcloud.msvc.usuarios.services.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -31,6 +30,26 @@ public class UsuarioController {
         }
         return ResponseEntity.notFound().build();
     }
+
+    @PostMapping
+    //@ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<?> crear(@RequestBody Usuario usuario){
+        return  ResponseEntity.status(HttpStatus.CREATED).body(service.guardar(usuario));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> editar(@RequestBody Usuario usuario, @PathVariable Long id){
+        Optional<Usuario> o = service.porId(id);
+        if(o.isPresent()){
+            Usuario usuarioDb = o.get();
+            usuarioDb.setNombre(usuario.getNombre());
+            usuarioDb.setEmail(usuario.getEmail());
+            usuarioDb.setPassword(usuario.getPassword());
+            return ResponseEntity.status(HttpStatus.CREATED).body(service.guardar(usuarioDb));
+        }
+        return ResponseEntity.notFound().build();
+    }
+
 
 
 }
