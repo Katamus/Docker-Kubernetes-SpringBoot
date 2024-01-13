d25e6da6fbd34cc77878d308bdfe3d5a243d2e97
criscahu <criscahu@hotmail.com>
2024-01-13 15:47:40 -0500

    29. Validando si existente el usuario en la BBDD

--- msvc-usuarios/src/main/java/org/catamus/springcloud/msvc/usuarios/controllers/UsuarioController.java
+++ msvc-usuarios/src/main/java/org/catamus/springcloud/msvc/usuarios/controllers/UsuarioController.java
@@ -9,10 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 
 @RestController
 public class UsuarioController {
@@ -38,6 +35,13 @@ public class UsuarioController {
     @PostMapping
     //@ResponseStatus(HttpStatus.CREATED)
     public ResponseEntity<?> crear(@Valid @RequestBody Usuario usuario, BindingResult result){
+
+        if(service.porEmail(usuario.getEmail()).isPresent()){
+            return ResponseEntity.badRequest()
+                    .body(Collections
+                            .singletonMap("mensaje","Ya existe un usuario con ese correo electronico!"));
+        }
+
         if(result.hasErrors()){
             return validar(result);
         }
@@ -46,12 +50,22 @@ public class UsuarioController {
 
     @PutMapping("/{id}")
     public ResponseEntity<?> editar(@Valid @RequestBody Usuario usuario, BindingResult result, @PathVariable Long id){
+
+
+
         if(result.hasErrors()){
             return validar(result);
         }
         Optional<Usuario> o = service.porId(id);
         if(o.isPresent()){
             Usuario usuarioDb = o.get();
+
+            if(!usuario.getEmail().equalsIgnoreCase(usuarioDb.getEmail()) && service.porEmail(usuario.getEmail()).isPresent()){
+                return ResponseEntity.badRequest()
+                        .body(Collections
+                                .singletonMap("mensaje","Ya existe un usuario con ese correo electronico!"));
+            }
+
             usuarioDb.setNombre(usuario.getNombre());
             usuarioDb.setEmail(usuario.getEmail());
             usuarioDb.setPassword(usuario.getPassword());
--- msvc-usuarios/src/main/java/org/catamus/springcloud/msvc/usuarios/repositories/UsuarioRepository.java
+++ msvc-usuarios/src/main/java/org/catamus/springcloud/msvc/usuarios/repositories/UsuarioRepository.java
@@ -3,5 +3,10 @@ package org.catamus.springcloud.msvc.usuarios.repositories;
 import org.catamus.springcloud.msvc.usuarios.models.entity.Usuario;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UsuarioRepository extends CrudRepository<Usuario,Long> {
+
+    Optional<Usuario> findByEmail(String email);
+
 }
