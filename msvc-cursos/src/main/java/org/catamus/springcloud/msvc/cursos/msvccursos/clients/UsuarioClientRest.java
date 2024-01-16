ebca40d10fc77313e7f023129c3733f272d4aeeb
criscahu <criscahu@hotmail.com>
2024-01-16 10:11:42 -0500

    85. Variables de ambiente (env) para los hostnames de los contenedores

--- msvc-cursos/src/main/java/org/catamus/springcloud/msvc/cursos/msvccursos/clients/UsuarioClientRest.java
+++ msvc-cursos/src/main/java/org/catamus/springcloud/msvc/cursos/msvccursos/clients/UsuarioClientRest.java
@@ -2,13 +2,12 @@ package org.catamus.springcloud.msvc.cursos.msvccursos.clients;
 
 import org.catamus.springcloud.msvc.cursos.msvccursos.models.Usuario;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Iterator;
 import java.util.List;
 
-@FeignClient(name="msvc-usuarios", url = "msvc-usuarios:8001")
+@FeignClient(name="msvc-usuarios", url = "${msvc.usuarios.url}")
 public interface UsuarioClientRest {
 
     @GetMapping("/{id}")
