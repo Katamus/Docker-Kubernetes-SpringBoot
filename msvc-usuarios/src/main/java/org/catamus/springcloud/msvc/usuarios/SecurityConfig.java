cbea2ff55e370ab04e5a9e1339b16ac20111bd08
criscahu <criscahu@hotmail.com>
2024-02-03 18:21:14 -0500

    184. Configurando OAuth2 Resource Server

--- msvc-usuarios/src/main/java/org/catamus/springcloud/msvc/usuarios/SecurityConfig.java
+++ msvc-usuarios/src/main/java/org/catamus/springcloud/msvc/usuarios/SecurityConfig.java
@@ -2,12 +2,15 @@ package org.catamus.springcloud.msvc.usuarios;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
+import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
 
 @EnableWebSecurity
 @Configuration
@@ -17,13 +20,21 @@ public class SecurityConfig {
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http.authorizeRequests(authz->
-                authz.anyRequest().authenticated()
+                authz
+                .requestMatchers(antMatcher("/authorized")).permitAll()
+                .requestMatchers(antMatcher(HttpMethod.GET,"/")).hasAnyAuthority("SCOPE_read","SCOPE_write")
+                .requestMatchers(antMatcher(HttpMethod.GET,"/{id}")).hasAnyAuthority("SCOPE_read","SCOPE_write")
+                .requestMatchers(antMatcher(HttpMethod.POST,"/")).hasAuthority("SCOPE_write")
+                .requestMatchers(antMatcher(HttpMethod.PUT,"/{id}")).hasAuthority("SCOPE_write")
+                .requestMatchers(antMatcher(HttpMethod.DELETE,"/{id}")).hasAuthority("SCOPE_write")
+                .anyRequest().authenticated()
         )
         .sessionManagement(session-> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .oauth2Login(
                 oauth2Login -> oauth2Login.loginPage("/oauth2/authorization/msvc-usuarios-client")
         )
-        .oauth2Client(withDefaults());
+        .oauth2Client(withDefaults())
+        .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()));
 
         return http.build();
     }
