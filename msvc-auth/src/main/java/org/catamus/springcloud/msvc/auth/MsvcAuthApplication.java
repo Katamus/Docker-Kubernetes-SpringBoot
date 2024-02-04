388dea681176787bd1f020cb339bca4ec141b89b
criscahu <criscahu@hotmail.com>
2024-02-03 20:01:29 -0500

    188. Aplicando configuraciones y cambios & probando con Postman

--- msvc-auth/src/main/java/org/catamus/springcloud/msvc/auth/MsvcAuthApplication.java
+++ msvc-auth/src/main/java/org/catamus/springcloud/msvc/auth/MsvcAuthApplication.java
@@ -1,4 +1,4 @@
-package org.catamus.springcloud.msvc.msvcauth;
+package org.catamus.springcloud.msvc.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
